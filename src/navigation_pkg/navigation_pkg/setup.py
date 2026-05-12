077249832fbcb6ef908d7898ce10b06b8f108c92
sjie128 <janisagoh128@gmail.com>
2026-05-12 13:26:34 +0800

    initial commit

---
+++ src/navigation_pkg/navigation_pkg/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'navigation_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='indigo128_',
+    maintainer_email='indigo128_@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+    'console_scripts': [
+        'camera_follower = navigation_pkg.camera_follower:main',
+        'normal_follower = navigation_pkg.normal_follower:main',
+    ],
+    },
+)
